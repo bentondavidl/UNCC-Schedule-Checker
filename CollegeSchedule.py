aff4d20c8b88066fce5a9a92cb8a1209e4e97df2
David <21026176+Delta-Dave@users.noreply.github.com>
2019-12-20 22:50:12 -0500

    Create class for full schedule

---
+++ CollegeSchedule.py
@@ -0,0 +1,12 @@
+import CollegeCourse
+
+class CollegeSchedule:
+    def __init__(self, courses):
+        self.courses = courses
+
+    # TODO: Add methods for counting classes per day
+    # TODO: Add methods for checking for overlapping courses
+    # TODO: Add methods for getting total credit credit hours
+    # TODO: Add methods for count of classes
+    # TODO: COOL STUFF!!
+        
